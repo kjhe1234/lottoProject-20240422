b06b572a7196c631d9018201c58d56ba57b8d0aa
kjhe1234 <kjhe1234@naver.com>
2024-04-22 11:45:06 +0900

    sqlAlchemy 데이터프레임을 db테이블로 만들기 예제 추가

---
+++ sqlAlchemyTest.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import pymysql
+
+data = {'학번': range(2000,2010), '성적':[70,60,100,90,50,75,85,99,78,63]}
+
+df = pd.DataFrame(data=data, columns=['학번','성적'])
+
+#print(df)
+
+engine = create_engine("mysql+pymysql://root:12345@localhost:3306/lottodb?charset=utf8mb4")
+engine.connect()
+df.to_sql(name="test_tbl", con=engine, if_exists='append', index=False)
+
+
+
+
+
